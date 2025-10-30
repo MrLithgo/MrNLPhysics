4f75e94a5dfb749834dfdf302b4a926607bea820
MrLithgo <nlithgo@gmail.com>
2025-10-30 06:50:59 +0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -43,17 +43,7 @@ const routes = [
         name: 'ALevelParticles',
         component: ALevelParticles
     },
-  {
-    path: '/quizzes',
-    name: 'quizzes',
-    component: QuizView
-  },
-  {
-    path: '/quiz/:id',
-    name: 'quiz',
-    component: QuizView,
-    props: true
-  }
+ 
 ]
 
 const router = createRouter({
