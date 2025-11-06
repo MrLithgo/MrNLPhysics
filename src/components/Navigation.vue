da704ee76a11f4ff6bc6be6071a31fbf8c9a6c97
MrLithgo <nlithgo@gmail.com>
2025-11-06 10:54:16 +0400

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,7 +17,7 @@
         </div>
        
         <div class="mobile-menu-toggle">
-          <button class="mobile-menu-btn" @click="toggleMobileMenu">
+          <button class="mobile-menu-btn" aria-label="mobile menu toggle" @click="toggleMobileMenu">
             <svg class="menu-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
